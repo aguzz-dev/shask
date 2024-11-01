0debe7f88a912fbaa1ee2b4d92b6cdff378dd705
AgustinTaverna <agustintaverna.developer@gmail.com>
2024-11-01 20:13:24 -0300

    Quita entorno docker

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-chown -R www-data:www-data /var/www/storage/
-chown -R www-data:www-data /var/www/bootstrap/cache/
-composer install
-composer dump-autoload
-su -s /bin/sh -c "php artisan optimize" www-data
-php artisan migrate
-
-service nginx start
