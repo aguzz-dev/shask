068da968421374f21b0a28b8302e7512bbe3cd2a
AguzzDev <agustintaverna.dev@gmail.com>
2024-12-25 19:23:04 -0300

    Revert "cirujeada"
    This reverts commit dbc79b537a16c2ea0f8a02cde3d9ba287e054260.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Database;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function(){
     return view('Download');
@@ -18,11 +17,3 @@ Route::get('/{id}', [QuestionController::class, 'sendQuestion']);
 
 //Endpoint para almacenar pregunta desde web
 Route::post('question/create-web', [QuestionController::class, 'storeQuestionFromWeb'])->middleware('throttle:2,0.5');
-
-
-Route::get('/888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888845', function(){
-    (new Database)->query("ALTER DATABASE railway CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
-});
-Route::get('/88888888888888888888888888888888888888888888888888888888888888888888877777777777777774521122', function(){
-    (new Database)->query("ALTER TABLE questions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
-});
