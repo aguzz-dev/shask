66d49fa9355388c24a2746eae36fe9975f3f5ab8
AguzzDev <agustintaverna.dev@gmail.com>
2024-12-26 23:11:13 -0300

    Arregla notificaciones

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -14,20 +14,22 @@ class NotificationController extends Controller
         $fcm = (new User)->getFcmByUsername($request->username)['fcm_token'];
         $description = $request->text;
 
-        $firebaseConfig = [
-            'type' => env('FIREBASE_TYPE'),
-            'private_key' => env('FIREBASE_PRIVATE_KEY'),
+        $firebaseCredentials = [
+            'type' => 'service_account',
             'project_id' => env('FIREBASE_PROJECT_ID'),
+            'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+            'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
             'client_email' => env('FIREBASE_CLIENT_EMAIL'),
             'client_id' => env('FIREBASE_CLIENT_ID'),
             'auth_uri' => env('FIREBASE_AUTH_URI'),
             'token_uri' => env('FIREBASE_TOKEN_URI'),
             'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
             'client_x509_cert_url' => env('FIREBASE_CLIENT_X509_CERT_URL'),
+            'universe_domain' => env('FIREBASE_UNIVERSE_DOMAIN'),
         ];
 
         $client = new GoogleClient();
-        $client->setAuthConfig($firebaseConfig);
+        $client->setAuthConfig($firebaseCredentials);
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
         $token = $client->fetchAccessTokenWithAssertion();
